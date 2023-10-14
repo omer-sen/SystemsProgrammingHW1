e478908b9723994dce3861e00ff0c85e1f0d0b82
Reuben Thomas <your@email.com>
2023-10-14 18:59:04 -0400

    with ptr arithmetic

--- mymalloc.c
+++ mymalloc.c
@@ -1,5 +1,6 @@
 #include <stdio.h> 
 #include <stdlib.h>
+#include <stdbool.h>
 #include <unistd.h>
 #include "mymalloc.h"
 
@@ -12,20 +13,67 @@ static double memory[MEMLENGTH];
 char *heapstart = (char *) memory;//heapstart will refer to the first byte of memory,
     // b/c it is memory casted as a char array.
 
-typedef struct Metadata{ //16 bytes
-    int size; //size of FULL chunk  ( meta+payload)
-    int state;
-    struct Metadata *next; //8 bytes
-    //struct Metadata *prev
-}meta;
+                // typedef struct Metadata{ //16 bytes
+                //     int size; //size of FULL chunk  ( meta+payload)
+                //     int state;
+                //     struct Metadata *next; //8 bytes
+                //     //struct Metadata *prev
+                // }meta;
 
-int metasize = sizeof(meta);
-static meta* header  = NULL;
-//int firstmalloc = 1; //need to initialize the first header on first malloc
+                // int metasize = sizeof(meta);
+                // static meta* header  = NULL;
+int firstmalloc = 1; //need to initialize the first header on first malloc
 
 //metadata and payload must both be multiples of 8
     //metadata should always be 8 bytes
+int getSize(char* ptr){
+    int* p = (int*) ptr;
+    return *p;
 
+}
+bool isFree(char*ptr){
+    int* p = (int*) (ptr+8);
+    return (*p==0);
+}
+bool setSize(char* ptr, int size){
+    int* p2 = (int*) ptr;
+    *p2 = size;
+    return  (*p2 = size);
+}
+bool setState(char* ptr, int state){
+    int* p2 = (int*) (ptr+8);
+    return (*p2 = state);
+}
+int getSizeNext(char* ptr){
+    ptr = getNext(ptr);
+    int *p = (int*) ptr;
+    return *p;
+    
+}
+
+bool isFreeNext(char* ptr){
+    ptr = getNext(ptr);
+    int* p = (int*) (ptr+8);
+    return (*p==0);
+}
+
+bool setSizeNext(char* ptr, int size){
+    ptr = getNext(ptr);
+    int* p = (int*) ptr;
+    return (*p=size);
+}
+
+bool setStateNext(char* ptr, int state){
+    ptr = getNext(ptr);
+    int* p = (int*) (ptr+8);
+    return (*p=state);
+}
+
+char* getNext(char* ptr){
+    int* p = (int*) ptr;
+    int size1 = *p;
+    return ptr + size1;
+}
 void* mymalloc(size_t size, char* file, int line){
     //if firstmalloc, place first meta block
     if(size==0){
@@ -34,44 +82,68 @@ void* mymalloc(size_t size, char* file, int line){
     }
 
     size= ROUNDUP8(size);
-    if(!header){
-        //inititialize first header on first malloc
-        header = (meta*) heapstart;
-        header->next = NULL;
-        header->size = MEMLENGTH;
-        header->state = FREE;
+                // if(!header){
+                //     //inititialize first header on first malloc
+                //     header = (meta*) heapstart;
+                //     header->next = NULL;
+                //     header->size = MEMLENGTH;
+                //     header->state = FREE;
 
-        //firstmalloc = 0;
+                //     //firstmalloc = 0;
 
-    }
-    meta* ptr = header;
+                // }
+    char* ptr = heapstart;
     //now look for chunk big enough to hold memory
     /* Conditions:
         if chunk is not big enough OR is not free go to next chunk
     */
-    while(ptr!=NULL){
-        if(ptr->size < (size + metasize) || ptr->state != FREE){
-            
-            ptr = ptr->next;
-        }else{
-            
-            //set up next metadata
-            ptr->next = (char*)ptr + size + metasize; //cast to char* so it's refering to 1 byte
-            ptr->next->state = FREE;        //^ size of ptr
-            ptr->next->size = ptr->size - size - metasize;
+    while(ptr<heapstart +MEMLENGTH){
+        bool IsFree = isFree(ptr);
+        int chunkSize = getSize(ptr);
 
-            ptr->state = ALOC;
-            ptr->size = metasize + size;
-            return (char*)ptr + metasize;
+        if(firstmalloc || (IsFree && chunkSize >= size+8) ){
+                                                //size for next metadata too
+            setSize(ptr, size);
+            setState(ptr, 1);
+            //set up next metadata
+            if(firstmalloc)
+                setSizeNext(ptr, MEMLENGTH - (size + 8));
+            else 
+                setSizeNext(ptr, chunkSize - (size+8));
+            
+            setStateNext(ptr, 1);
+            firstmalloc = 0;
+            return ptr+8;
+
+                // if(ptr->size < (size + metasize) || ptr->state != FREE){
+                    
+                //     ptr = ptr->next;
+                // }else{
+                    
+                //     //set up next metadata
+                //     ptr->next = (char*)ptr + size + metasize; //cast to char* so it's refering to 1 byte
+                //     ptr->next->state = FREE;        //^ size of ptr
+                //     ptr->next->size = ptr->size - size - metasize;
+
+                //     ptr->state = ALOC;
+                //     ptr->size = metasize + size;
+                //     return (char*)ptr + metasize;
+                // }
+        }else{
+            ptr= getNext(ptr);
         }
+
     }
 
     //not enough space 
-    printf("Not enough space for %d bytes, requested in file %s at line %d", size, file, line);
+    printf("Not enough space for %zu BYTES, requested in FILE %s at LINE %d", size, file, line);
     return NULL;
 
 }
 
 void* myfree(void* ptr,char* file, int line){
     //probably really handy to have a prev pointer for this
+    //need to coalesce too
+    
+
 }
--- mymalloc.h
+++ mymalloc.h
@@ -6,5 +6,14 @@
 
 void* mymalloc(size_t size, char* file, int line);
 void* myfree(void* ptr,char* file, int line);
+int getSize(char* ptr);
+bool isFree(char*ptr);
+bool setSize(char* ptr, int size);
+bool setState(char* ptr, int state);
+int getSizeNext(char* ptr);
+bool isFreeNext(char* ptr);
+bool setSizeNext(char* ptr, int size);
+bool setStateNext(char* ptr, int state);
+char* getNext(char* ptr);
 
 #endif
