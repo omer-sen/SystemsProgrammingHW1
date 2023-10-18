866cee7e25587273399c2447a81b0eb4698404fa
omer-sen <ofsen03@gmail.com>
2023-10-18 12:08:56 -0400

    Finished

--- memgrind.c
+++ memgrind.c
@@ -25,11 +25,69 @@ bool test2(){
     char* arr[120];
     for(int i=0; i<120; i++){
         arr[i] = malloc(1);
+        if(arr[i]==NULL){
+            return false;
+        }
     }
     for(int i=0; i<120; i++){
         free(arr[i]);
     }
     return memCleared() ==1;
+}
+
+bool test3(){
+    int random_number;
+    char* arr[120];
+    int counter = 0;
+    while(counter<120){
+        random_number = rand() % 2;
+        if(random_number == 1){
+            arr[counter] = malloc(1);
+            if(arr[counter]==NULL){
+                return false;
+            }
+            counter++;
+        }else{
+            if(counter>0 && arr[counter] != NULL){
+                counter--;
+                free(arr[counter]);
+            }
+        }
+        
+    }
+    for(int i=0; i<120; i++){
+        if(arr[i] != NULL)
+            free(arr[i]);
+    }
+    return memCleared() ==1;
+}
+
+bool test4(){
+    char* arr[MEMLENGTH/2];
+    for(int i=0; i<120; i++){
+        arr[i] = malloc(1);
+    }
+    for(int i=0; i<120; i++){
+        free(arr[i]);
+    }
+    char *ptr = malloc(MEMLENGTH-16);
+    free(ptr);
+    return memCleared() == 1;
+}
+
+bool test5(){
+    int random_number;
+    for(int i=0; i<120; i++){
+        random_number = rand() % 2;
+        if(random_number == 1){ 
+            char *ptr = malloc(MEMLENGTH-16);
+            free(ptr);
+        }else{
+            char *ptr = malloc(1);
+            free(ptr);
+        }
+    }
+    return memCleared() == 1;
 }
 
 int main(){
@@ -40,24 +98,77 @@ int main(){
     //test1
     for(int i = 0; i < 50; i++) {
         clock_t start = clock();
-        test1();
+        if(test1()==false){
+            printf("error");
+            return EXIT_SUCCESS;
+        }
         clock_t end = clock();
         double time = (double) (end-start) / CLOCKS_PER_SEC;
         timeSum+=time;
     }
     double avgTime =timeSum/50;
     printf("Average time for test1: %f\n", avgTime);
+    //clear memory
 
-     //test2
+    //test2
+    timeSum=0;
     for(int i = 0; i < 50; i++) {
         clock_t start = clock();
-        test2();
+        if(test2()==false){
+            printf("error");
+            return EXIT_SUCCESS;
+        }
         clock_t end = clock();
-        double time2 = (double) (end-start) / CLOCKS_PER_SEC;
-        timeSum+=time2;
+        double time = (double) (end-start) / CLOCKS_PER_SEC;
+        timeSum+=time;
     }
-    double avgTime2 =timeSum/50;
-    printf("Average time for test1: %f\n", avgTime2);
+    avgTime =timeSum/50;
+    printf("Average time for test2: %f\n", avgTime);
 
+     //test3
+    timeSum=0;
+    for(int i = 0; i < 50; i++) {
+        clock_t start = clock();
+        if(test3()==false){
+            printf("error");
+            return EXIT_SUCCESS;
+        }
+        clock_t end = clock();
+        double time = (double) (end-start) / CLOCKS_PER_SEC;
+        timeSum+=time;
+    }
+    avgTime =timeSum/50;
+    printf("Average time for test3: %f\n", avgTime);
+
+    //test4
+    timeSum=0;
+    for(int i = 0; i < 50; i++) {
+        clock_t start = clock();
+        if(test4()==false){
+            printf("error");
+            return EXIT_SUCCESS;
+        }
+        clock_t end = clock();
+        double time = (double) (end-start) / CLOCKS_PER_SEC;
+        timeSum+=time;
+    }
+    avgTime =timeSum/50;
+    printf("Average time for test4: %f\n", avgTime);
+
+    //test5
+    timeSum=0;
+    for(int i = 0; i < 50; i++) {
+        clock_t start = clock();
+        if(test5()==false){
+            printf("error");
+            return EXIT_SUCCESS;
+        }
+        clock_t end = clock();
+        double time = (double) (end-start) / CLOCKS_PER_SEC;
+        timeSum+=time;
+    }
+    avgTime =timeSum/50;
+    printf("Average time for test5: %f\n", avgTime);
+    
     return EXIT_SUCCESS;
 }
--- mymalloc.c
+++ mymalloc.c
@@ -1,12 +1,10 @@
-
-
 #include <stdio.h> 
 #include <stdlib.h>
 #include <stdbool.h>
 #include <unistd.h>
 #include "mymalloc.h"
 
-#define MEMLENGTH 512
+#define MEMLENGTH 2048
 #define FREE 1
 #define ALOC 0
 #define ROUNDUP8(x) (((x) + 7) & (-8))
@@ -14,14 +12,9 @@
 static double memory[MEMLENGTH];
 char *heapstart = (char *) memory;//heapstart will refer to the first byte of memory,
  
- struct MetaData {
-    size_t size;
-    int is_allocated;
-    struct MetaData* next;
-};
-
 bool validPtr(char* ptr){
     return (ptr<heapstart+MEMLENGTH) && (ptr>=heapstart);
+    //PPOINTER MUST ALSO BE AT THE START OF A PAYLOAD!!!
 }
 int getSize(char* ptr){
     int* p = (int*) ptr;
@@ -32,14 +25,13 @@ bool isFree(char*ptr){
     int* p = (int*) (ptr+4);
     return (*p==1);
 }
-bool setSize(char* ptr, int size){
+void setSize(char* ptr, int size){
     int* p2 = (int*) ptr;
     *p2 = size;
-    return  (*p2 = size);
 }
-bool setState(char* ptr, int state){
+void setState(char* ptr, int state){
     int* p2 = (int*) (ptr+4);
-    return (*p2 = state);
+    *p2 = state;
 }
 int getSizeNext(char* ptr){
     int* p = (int*) getNext(ptr);
@@ -54,16 +46,16 @@ bool isFreeNext(char* ptr){
     return (*p==1);
 }
 
-bool setSizeNext(char* ptr, int size){
+void setSizeNext(char* ptr, int size){
     //ptr = getNext(ptr);
     int* p = (int*) getNext(ptr);
-    return (*p=size);
+    *p=size;
 }
 
-bool setStateNext(char* ptr, int state){
+void setStateNext(char* ptr, int state){
     //ptr = getNext(ptr);
     int* p = (int*) (getNext(ptr)+4);
-    return (*p=state);
+    *p=state;
 }
 
 char* getNext(char* ptr){
@@ -71,53 +63,59 @@ char* getNext(char* ptr){
     int size1 = *p;
     return ptr + size1;
 }
+void* mymalloc(size_t size, char* file, int line){
+    if(size==0){
+        printf("Cannot allocate 0 bytes");
+        return NULL;
+    }
+    size= ROUNDUP8(size); //metadata should always be a multiple of 8 bytes
 
+    char* ptr = heapstart;
+    while(ptr<heapstart +MEMLENGTH){
+        bool IsFree = isFree(ptr);
+        int chunkSize = getSize(ptr);
+
+        if(chunkSize==0 || (IsFree && chunkSize >= size+16) ){//space for current+next metadata (8+8)
+            setSize(ptr, size+8);
+            setState(ptr, 0);
+            //set up next metadata
+            if(chunkSize==0)
+                setSizeNext(ptr, MEMLENGTH - (size + 8));
+            else{ 
+                if(chunkSize - (size+8) == 8){ 
+                //if chunkSize only has space for payload and new metadata (ie new metadata is useless), 
+                // then just give that space to payload
+                    setSize(ptr, getSize(ptr) + 8);
+                    return ptr+8;
+                }else
+                    setSizeNext(ptr, chunkSize - (size+8));
+            }
+            setStateNext(ptr, 1);
+            return ptr+8;      
+        }else{
+            ptr= getNext(ptr);
+        }
+    }
+    //not enough space 
+    printf("Not enough space for %zu BYTES, requested in FILE %s at LINE %d\n", size, file, line);
+    return NULL;
+}
 bool isPrecedingAndFree(char* curr, char* ptr){
     return (getNext(curr) == ptr) && (isFree(curr));
 }
-bool mergeBlocks(char* p1, char* p2){
+void mergeBlocks(char* p1, char* p2){
     //merge to p1
     int size1 = getSize(p1);
     int size2 = getSize(p2);
-    return (setSize(p1, size1+size2));
+    setSize(p1, size1+size2);
 }
-
-void* mymalloc(size_t size, char* file, int line) {
-    
-    size_t aligned_size = ROUNDUP8(size);
-
-    struct MetaData* current = (struct MetaData*)memory;
-    
-    if(!(current->size > 0)){
-        current->size=MEMLENGTH;
-    }
-    while(current != NULL){
-        if(!current->is_allocated && current->size >= aligned_size){
-            current->is_allocated = 1; 
-            current->size = aligned_size;
-            if(current->next == NULL){
-                
-                current->next = (struct MetaData*) current + aligned_size;
-                current->next->is_allocated = FREE;
-                current->next->size = MEMLENGTH - (aligned_size + sizeof(struct MetaData));
-            }
-            
-            return (void*)current + sizeof(struct MetaData);
-            
-        }
-        current = current->next;
-    }
-
-    return NULL;
-}
-
 void* myfree(void* ptr,char* file, int line){
     if(!validPtr(ptr)){
         printf("Error: Attempted to free a pointer that was not obtained from heap, in FILE %s at LINE %d\n", file, line);
         return NULL;
     }
     char* curr = heapstart;
-    char* realPtr = ptr - sizeof(struct MetaData);
+    char* realPtr = ptr - 8;
 
     while(curr< heapstart+MEMLENGTH){
         if(isPrecedingAndFree(curr, realPtr)){
